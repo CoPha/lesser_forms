0e4f0209d78cb8f253861b17b718935fd32fd88b
bertdlf <bertdlf@3060063.no-reply.drupal.org>
2015-09-24 00:03:26 +0200

    Lf-7 creation of config screen

--- src/Controller/LesserFormsController.php
+++ src/Controller/LesserFormsController.php
@@ -5,6 +5,7 @@
  */
 
 namespace Drupal\lesser_forms\Controller;
+use Drupal\user\Entity\Role;
 
 /**
  * LesserFormsController.
@@ -15,12 +16,33 @@ class LesserFormsController {
    */
   public function config() {
 
-    $settings = \Drupal::config('lesser_forms.settings')
-      ->get('lesser_forms_config');
+    $settings = \Drupal::config('lesser_forms.settings')->get('lesser_forms_config');
 
+    $user_roles = Role::loadMultiple();
 
-    return array(
-      '#markup' => var_dump($settings),
+    $header = array(t('field'));
+
+    foreach ($user_roles as $id => $entity) {
+        $header[]=$entity->label();
+    }
+    $form['lf_table'] = array(
+        '#type' => 'table',
+        '#header' => $header,
     );
+    $fields = array('promote','sticky','preview','author','revision_information');
+
+    foreach ($fields as $field) {
+      $form['lf_table'][$field]['title'] = array(
+          '#plain_text' => $field,
+      );
+      foreach ($user_roles as $id => $entity) {
+        $form['lf_table'][$field][$entity->label()] = array(
+           '#type' => 'checkbox',
+           '#default_value' => '',
+        );
+      }
+    }
+
+    return $form;
   }
 }
